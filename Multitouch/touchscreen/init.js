bcbd88adacf8349f37c979e3d0e3365816f7e753
Michael Fynes <michaef@metropolia.fi>
2014-03-13 12:11:37 +0200

    this time should be reverted properly

--- Multitouch/touchscreen/init.js
+++
@@ -1,6 +0,0 @@
-var timer = setInterval(function() {
-   if ($('.aw-widget-content').length == 1) {
-       $('.aw-widget-content').attr('style','border: none !important');
-       clearInterval(timer);
-   }
-}, 200);
--- hammer/js/touchscreen.js
+++ hammer/js/touchscreen.js
@@ -30,7 +30,21 @@ $('document').ready(function() {
 	/* Canvas functionality */
 	$('.wrap').hammer().on("tap", function(event) {
 		if(menuOpen)
-			closeMenu();
+		{
+			/*
+			Hammer(currentMenu.parent().children('#reset')).off('hold tap doubletap drag dragstart dragend dragup dragdown dragleft dragright swipe swipeup swipedown swipeleft swiperight transform transformstart transformend rotate pinch pinchin pinchout touch release');
+			
+			Hammer(currentMenu.parent().children('#edit')).off('hold tap doubletap drag dragstart dragend dragup dragdown dragleft dragright swipe swipeup swipedown swipeleft swiperight transform transformstart transformend rotate pinch pinchin pinchout touch release');
+			*/
+		
+			Hammer(currentMenu.parent().children('#delete')).off('hold tap doubletap drag dragstart dragend dragup dragdown dragleft dragright swipe swipeup swipedown swipeleft swiperight transform transformstart transformend rotate pinch pinchin pinchout touch release');
+			
+			currentMenu.remove();
+			
+			menuOpen = false;
+			
+			/* NOT COMPLETE ! */
+		}
 	});
 	
 	$('.wrap').hammer().on("doubletap", function(event) {
@@ -184,9 +198,6 @@ $('document').ready(function() {
 		$('#item_' + itemID).hammer().on("tap", function(event) {
 			event.stopPropagation();
 			
-			if(menuOpen)
-				closeMenu();
-			
 			/* Set z-index to be the highest available so that the note pops up on front when tapped */
 			if($(event.target).css('z-index') != zIndex-1)
 			{
@@ -199,44 +210,41 @@ $('document').ready(function() {
 		$('#item_' + itemID).hammer().on("hold", function(event) {
 			event.stopPropagation();
 			
-			if(!menuOpen)
-			{
-				menuOpen = true;
+			menuOpen = true;
+			
+			/* Append menu to body (Needs optimizing html part)*/ 
+			$('body').append('<div id="item_'+ $(event.target).attr('data-id') + '_menu" class="menu" data-id="'+ $(event.target).attr('data-id') +'"><ul><li id="edit">EDIT</li><li id="reset">RESET</li><li id="delete">DELETE</li></ul></div>');
+			
+			/* Store the opened menu to a variable for later use */
+			currentMenu = $('#item_'+ $(event.target).attr('data-id') + '_menu');
+			
+			/* Add menu button functions on tap */
+			$('#item_'+ $(event.target).attr('data-id') + '_menu').find('#delete').hammer().on("tap", function(event) {
+				event.stopPropagation();
 				
-				/* Append menu to body (Needs optimizing html part)*/ 
-				$('body').append('<div id="item_'+ $(event.target).attr('data-id') + '_menu" class="menu" data-id="'+ $(event.target).attr('data-id') +'"><ul><li id="edit">EDIT</li><li id="reset">RESET</li><li id="delete">DELETE</li></ul></div>');
+				/* Removing all event listeners related to the note */
+				Hammer($('#item_'+ $(event.target).parent().parent().attr('data-id'))).off('hold tap doubletap drag dragstart dragend dragup dragdown dragleft dragright swipe swipeup swipedown swipeleft swiperight transform transformstart transformend rotate pinch pinchin pinchout touch release');
 				
-				/* Store the opened menu to a variable for later use */
-				currentMenu = $('#item_'+ $(event.target).attr('data-id') + '_menu');
+				/* Removing all event listeners related to the menu */
 				
-				/* Add menu button functions on tap */
-				$('#item_'+ $(event.target).attr('data-id') + '_menu').find('#delete').hammer().on("tap", function(event) {
-					event.stopPropagation();
-					
-					/* Removing all event listeners related to the note */
-					Hammer($('#item_'+ $(event.target).parent().parent().attr('data-id'))).off('hold tap doubletap drag dragstart dragend dragup dragdown dragleft dragright swipe swipeup swipedown swipeleft swiperight transform transformstart transformend rotate pinch pinchin pinchout touch release');
-					
-					/* Removing all event listeners related to the menu */
-					
-					/* Activate below when events are done (Create a loop to close events of children) */
-					/*
-					Hammer($(event.target).parent().children('#reset')).off('hold tap doubletap drag dragstart dragend dragup dragdown dragleft dragright swipe swipeup swipedown swipeleft swiperight transform transformstart transformend rotate pinch pinchin pinchout touch release');
-					
-					Hammer($(event.target).parent().children('#edit')).off('hold tap doubletap drag dragstart dragend dragup dragdown dragleft dragright swipe swipeup swipedown swipeleft swiperight transform transformstart transformend rotate pinch pinchin pinchout touch release');
-					*/
-					
-					Hammer($(event.target)).off('hold tap doubletap drag dragstart dragend dragup dragdown dragleft dragright swipe swipeup swipedown swipeleft swiperight transform transformstart transformend rotate pinch pinchin pinchout touch release');
-					
-					/* Removing the note from canvas */
-					$('#item_'+ $(event.target).parent().parent().attr('data-id')).remove();
-					
-					/* Closing menu */
-					$(event.target).parent().parent().remove();
-					menuOpen = false;
-				});
+				/* Activate below when events are done (Create a loop to close events of children) */
+				/*
+				Hammer($(event.target).parent().children('#reset')).off('hold tap doubletap drag dragstart dragend dragup dragdown dragleft dragright swipe swipeup swipedown swipeleft swiperight transform transformstart transformend rotate pinch pinchin pinchout touch release');
 				
-				$('#item_'+ $(event.target).attr('data-id') + '_menu').css('left',event.gesture.center.pageX).css('top',event.gesture.center.pageY);
-			}
+				Hammer($(event.target).parent().children('#edit')).off('hold tap doubletap drag dragstart dragend dragup dragdown dragleft dragright swipe swipeup swipedown swipeleft swiperight transform transformstart transformend rotate pinch pinchin pinchout touch release');
+				*/
+				
+				Hammer($(event.target)).off('hold tap doubletap drag dragstart dragend dragup dragdown dragleft dragright swipe swipeup swipedown swipeleft swiperight transform transformstart transformend rotate pinch pinchin pinchout touch release');
+				
+				/* Removing the note from canvas */
+				$('#item_'+ $(event.target).parent().parent().attr('data-id')).remove();
+				
+				/* Closing menu */
+				$(event.target).parent().parent().remove();
+				menuOpen = false;
+			});
+			
+			$('#item_'+ $(event.target).attr('data-id') + '_menu').css('left',event.gesture.center.pageX).css('top',event.gesture.center.pageY);
 		});
 		
 		/* When a new note is added add one to ID and z-index values in order to keep them unique */
@@ -244,25 +252,6 @@ $('document').ready(function() {
 		zIndex++;
 				
     });
-	
-	
-	/* Function for closing the menu so it can be easily called without copying all the functionality in multiple places */
-	function closeMenu()
-	{
-		/*
-		Hammer(currentMenu.parent().children('#reset')).off('hold tap doubletap drag dragstart dragend dragup dragdown dragleft dragright swipe swipeup swipedown swipeleft swiperight transform transformstart transformend rotate pinch pinchin pinchout touch release');
-		
-		Hammer(currentMenu.parent().children('#edit')).off('hold tap doubletap drag dragstart dragend dragup dragdown dragleft dragright swipe swipeup swipedown swipeleft swiperight transform transformstart transformend rotate pinch pinchin pinchout touch release');
-		*/
-	
-		Hammer(currentMenu.parent().children('#delete')).off('hold tap doubletap drag dragstart dragend dragup dragdown dragleft dragright swipe swipeup swipedown swipeleft swiperight transform transformstart transformend rotate pinch pinchin pinchout touch release');
-		
-		currentMenu.remove();
-		
-		menuOpen = false;
-		
-		/* NOT COMPLETE ! */
-	}
 	
 	/* Helper function got from Stackoverflow for rotation */
 	/* http://stackoverflow.com/questions/15191058/css-rotation-cross-browser-with-jquery-animate */
